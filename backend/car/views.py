44b9c3d3c3dee3c1b3dffc4bc0fb2742d8cb286d
Mehmet Ugur SATIR <musatir@gmail.com>
2023-04-27 14:06:35 +0300

    users lists only availabe cars

--- backend/car/views.py
+++ backend/car/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+from .models import Car, Reservation
+from .serializers import CarSerializer
 # Create your views here.
+
+
+class CarView(ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            return queryset
+        queryset = queryset.filter(is_available=True)
+
+        return queryset
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('users/', include('users.urls')),
+    path('api/', include('car.urls')),
 
     # Url paths for swagger:
     path("swagger(<format>\.json|\.yaml)",
