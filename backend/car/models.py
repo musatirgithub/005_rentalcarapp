f31dbe5638e9f1541de1d897f58b3545918de727
Mehmet Ugur SATIR <musatir@gmail.com>
2023-04-27 10:46:39 +0300

    Car and Reservation models cerated

--- backend/car/models.py
+++ backend/car/models.py
@@ -1,3 +1,32 @@
+from typing import Collection, Optional
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+GEAR = (
+    ('a', 'automatic'),
+    ('m', 'manuel')
+)
+
+
+class Car(models.Model):
+    plate_number = models.CharField(max_length=15)
+    brand = models.CharField(max_length=15)
+    model = models.CharField(max_length=15)
+    year = models.SmallIntegerField()
+    gear = models.CharField(max_length=1, choices=GEAR)
+    rent_per_day = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField()
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reservations_user')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE,
+                            related_name='reservations_car')
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        constraints = models.UniqueConstraint(
+            fields=('user', 'start_date', 'end_date'), name='same_res_notallowed')
